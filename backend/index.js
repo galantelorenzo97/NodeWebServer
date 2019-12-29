c9c28303d57e995ec4e481be051ac2c32532f40c
Lorenzo <galantelorenzo97@gmail.com>
2019-12-28 23:25:52 -0500

    Added another backend page

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+app.get('/test', function(req, res) {
+    res.send('TEST PAGE!');
+});
+
 var server = app.listen(3001, function() {});
