563b122002b41b6b9d2efcae7334506cac020585
Lorenzo <galantelorenzo97@gmail.com>
2019-12-28 23:12:05 -0500

    Fixed index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
 
-app.listen(3000, () =>
-    console.log('Example app listening on port 3000!'),
-);
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3001, function() {});
